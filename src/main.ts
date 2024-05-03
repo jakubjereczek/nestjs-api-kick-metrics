4bcc84d250ee63938fcd6ee6d4920b9215da0d33
jakubjereczek <99jakubjereczek@gmail.com>
2024-05-03 19:01:49 +0200

    setup swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const document = SwaggerModule.createDocument(
+    app,
+    new DocumentBuilder().setTitle('KickMetrics API').setVersion('1.0').build(),
+  );
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
